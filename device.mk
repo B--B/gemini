1a7f9276554ba7ba8b2c252a6808fe7179302ffa
xNombre <kartapolska@gmail.com>
2019-10-29 11:58:45 +0100

    gemini: Add fingerprint service
    Change-Id: Iafcee873f59a4e5bbc1c605555828aa71f7cac30
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.gemini
 
 # Input
 PRODUCT_COPY_FILES += \
