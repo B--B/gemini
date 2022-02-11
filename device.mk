b2823ba3004e4b6acf84dae50bf40726932a88ca
Marco Zanin <mrczn.bb@gmail.com>
2022-02-11 19:01:11 +0100

    Build pixel dance and add init config
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- device.mk
+++ device.mk
@@ -77,6 +77,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
+# Pixel Dance
+PRODUCT_PACKAGES += \
+    pixel.dance.sh
+
 # TFA calibration
 PRODUCT_PACKAGES += \
     init.tfa.sh \
