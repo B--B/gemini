c7ed17df6b40fc955e4c23ce0878e138abe153a5
Marco Zanin <mrczn.bb@gmail.com>
2023-01-27 00:09:05 +0100

    Gemini: bring back fp custom service and hex edited blob from my twelve branch
    Also uprev to v2.3 and update code for android 13
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- device.mk
+++ device.mk
@@ -52,7 +52,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.3-service.gemini
 
 # Input
 PRODUCT_COPY_FILES += \
